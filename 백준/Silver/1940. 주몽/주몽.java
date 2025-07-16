f5ef107bc77963e8649b2f52c69e304646f8675d
junhyuk1014 <169124397+junhyuk1014@users.noreply.github.com>
2025-07-16 19:26:50 +0900

    [Silver IV] Title: 주몽, Time: 396 ms, Memory: 35528 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int count = 0;
+        int start_idx = 0;
+        int end_idx = n - 1;
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        while(end_idx != start_idx){
+            if(arr[start_idx] + arr[end_idx] == m){
+                count++;
+                start_idx ++;
+            }
+            else if(arr[start_idx] + arr[end_idx] > m){
+                end_idx--;
+                }
+
+            else{
+                start_idx++;
+            }
+        }
+
+    System.out.println(count);
+
+
+    }
+}
